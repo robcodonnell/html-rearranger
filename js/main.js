90687c0b7b7c5ff1ec8eb1a5c05f7f1a24618a23
robcodonnell <robodonnell@Robs-MacBook-Pro.local>
2016-03-11 16:37:14 -0500

    stuff

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $ulBelow = $('<ul>');
+var $ulAbove = $('<ul>');
+
+$('body').append('<h1>Below ground veggies</h1>');
+
+$('body').append('<h1>Above ground veggies</h1>');
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $ulBelow.append($(this));
+  } else {
+    $ulAbove.append($(this));
+  }
+});
+
+$('ul:first-child').remove();
